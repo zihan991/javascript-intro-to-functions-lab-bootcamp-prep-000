91644dfde862b693ef38693bc7986e4fd1f442af
celestial-applet-1261 <zihan991@gmail.com>
2021-03-10 01:54:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,4 +14,6 @@ function sayHiToGrandma(string){
   if (string.toLowerCase() === string)
 return "I can't hear you!";
 } 
-else {
+else if (string.toUpperCase() === string) {
+  return "YES INDEED!";
+}
